bf2ce1b6566f39db4b180cb2baebe1ca0a1e8a81
Harlan <harlan.setia@gmail.com>
2024-12-31 15:30:09 +0700

    Change page

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-## About This Project
+## About This Project (Version 2.0)
+
 
 You can see my Portfolio [here](https://harlansr.github.io). 
---
+++ assets/js/data.js
@@ -0,0 +1,284 @@
+const data = {
+    education : {
+        en: [
+            {
+                name:"Andalas Univercity",
+                major: "Economics",
+                time: "2013-2024",
+            },
+            {
+                name:"Binar Academy",
+                major: "Full-Stack Web",
+                time: "2022 - 2023",
+            },
+        ],
+        id: [
+            {
+                name:"Universitas Andalas",
+                major: "Ilmu Ekonomi",
+                time: "2013-2024",
+            },
+            {
+                name:"Binar Academy",
+                major: "Full-Stack Web",
+                time: "2022 - 2023",
+            },
+        ],
+    },
+    job : {
+        en: [
+            {
+                name:"PT.Inti Talenta Teknologi",
+                position: "Technical Officer",
+                time: "Jan 2024 - Now"
+            },
+            {
+                name:"Badan Keuangan Kota Bukittinggi",
+                position: "Software Enginer",
+                time: "Apr 2022 - Jan 2024"
+            },
+            {
+                name:"Freelance",
+                position: "Software Enginer",
+                time: "2015 - 2023"
+            },
+            {
+                name:"Neo Telemetri",
+                position: "Interactive Multimedia Division",
+                time: "2014 - 2016"
+            },
+        ],
+        id: [
+            {
+                name:"PT.Inti Talenta Teknologi",
+                position: "Technical Officer",
+                time: "Jan 2024 - Sekarang"
+            },
+            {
+                name:"Badan Keuangan Kota Bukittinggi",
+                position: "Software Enginer",
+                time: "Apr 2022 - Jan 2024"
+            },
+            {
+                name:"Freelance",
+                position: "Software Enginer",
+                time: "2015 - 2023"
+            },
+            {
+                name:"Neo Telemetri",
+                position: "Interactive Multimedia Division",
+                time: "2014 - 2016"
+            },
+        ],
+    },
+    project : {
+        en: [
+            {
+                name:"Sistem Pengelolaan Pendapatan Daerah",
+                type: "website",
+                image: "https://harlansr.github.io/assets/img/portfolio/website/web_sppd.png",
+                video: null,
+                description: "A website that aims to manage taxation for the city of Bukittinggi, this project was worked on with a team but in this project I played a lot of roles, concepts, database design, boilerplate, ui/ux design, security, how to communicate with banks, how to communicate with Badan Pemeriksa Keuangan and many more.",
+                link: "https://sppd.bukittinggikota.go.id/",
+                tag:[
+                    "PHP", "Laravel", "SQL Server", "Postgre"
+                ]
+            },
+            {
+                name:"Pojok Pajak Bukittinggi",
+                type: "website",
+                image: "https://harlansr.github.io/assets/img/portfolio/website/web_keuangan.png",
+                video: null,
+                description: "A website-based application that aims to connect the Bukittinggi City Financial Institution with residents of the City of Bukittinggi, there you can check and pay taxes directly on the application. In this project I worked alone and it has been completed, but it has not yet been launched because it is still in preparation",
+                link: "https://pojokpajak.bukittinggikota.go.id/",
+                tag:[
+                    "PHP", "Laravel", "MySQL"
+                ]
+            },
+            {
+                name:"ECHAMP.ID",
+                type: "website",
+                image: "https://harlansr.github.io/assets/img/portfolio/website/web_echamp.png",
+                video: null,
+                description: "I am is single developer who made this website",
+                link: null,
+                tag:[
+                    "JavaScript", "Next.JS", "Express.JS", "Postgres"
+                ]
+            },
+            {
+                name:"Pojok Pajak Bukittinggi",
+                type: "mobile",
+                image: "https://harlansr.github.io/assets/img/portfolio/mobile/pojokpajak.jpg",
+                video: null,
+                description: "The application for connecting the Bukittinggi City Financial Institution with the residents of the city of Bukittinggi, this project was done by myself, after finishing building the website version, this application was developed into a mobile form so that usage becomes more effective, but this application has not yet been launched because it is still making preparations",
+                link: null,
+                tag:[
+                    "JavaScript", "ReactNative.JS", "Express.JS", "Postgres"
+                ]
+            },
+            {
+                name:"UNAND Link",
+                type: "mobile",
+                image: null,
+                video: "https://www.youtube.com/watch?v=sjrpUE_yS0U",
+                description: "This is a mobile-based application that helps all student activities while on campus, from class groups that they can follow in the application, get direct announcements from the organizations they follow, register for seminars and others, in building this application I built it myself, on the first day of launching this application got 3000 downloads and was a great success, unfortunately this application was no longer active because of my busy studies at that time",
+                link: null,
+                tag:[
+                    "Java", "Android Studio", "PHP", "MySQL"
+                ]
+            },
+            {
+                name:"Cash Register",
+                type: "desktop",
+                image: "https://harlansr.github.io/assets/img/portfolio/desktop/desktop-restorant.png",
+                video: null,
+                description: "A desktop-based cashier application installed on a dedicated machine, which directly sends data to the local tax management database. I worked on this project individually.",
+                link: null,
+                tag:[
+                    "JavaScript", "Electron.JS", "Express.JS", "Postgres"
+                ]
+            },
+            {
+                name:"Absensi Visitor Dilo Padang",
+                type: "desktop",
+                image: "https://harlansr.github.io/assets/img/portfolio/desktop/absen-dilo.png",
+                video: null,
+                description: "Dilo Padang is a subsidiary of PT Telkom Indonesia (Persero) Tbk asking personally to make attendance software for visitors who come, visitors only need to scan the QR code on their member card to take attendance, this attendance software can immediately generate reports and can export excel directly from the application, in this project I worked alone",
+                link: null,
+                tag:[
+                    "C#", ".NET"
+                ]
+            },
+            {
+                name:"Project to Escape",
+                type: "game",
+                image: "https://harlansr.github.io/assets/img/portfolio/game/game_pte.jpg",
+                video: null,
+                description: "I am is single developer who made this game",
+                link: "https://store.steampowered.com/app/1612220/Project_to_Escape/",
+                tag:[
+                    "C#", "UnityEngine", "Blender"
+                ]
+            },
+            {
+                name: "Custom E-Mony",
+                type: "2d",
+                image: "https://harlansr.github.io/assets/img/portfolio/design/mockup-emoney.png",
+                video: null,
+                description: null,
+                link: null,
+                tag:[ ]
+            },
+            {
+                name: "Poster Pojok Pajak",
+                type: "2d",
+                image: "https://harlansr.github.io/assets/img/portfolio/mobile/pojokpajak.jpg",
+                video: null,
+                description: null,
+                link: null,
+                tag:[ ]
+            },
+            {
+                name: "Poster E-champion PUBG",
+                type: "2d",
+                image: "https://harlansr.github.io/assets/img/portfolio/design/echampion_1.jpg",
+                video: null,
+                description: null,
+                link: null,
+                tag:[ ]
+            },
+            {
+                name: "Poster E-champion Valorant",
+                type: "2d",
+                image: "https://harlansr.github.io/assets/img/portfolio/design/beta-turnament-poster.png",
+                video: null,
+                description: null,
+                link: null,
+                tag:[ ]
+            },
+            {
+                name: "Design Project to Escape",
+                type: "3d",
+                image: "https://harlansr.github.io/assets/img/portfolio/3d/ed_pte_art.jpg",
+                video: null,
+                description: null,
+                link: null,
+                tag:[ ]
+            },
+            {
+                name: "Design 3D",
+                type: "3d",
+                image: "https://harlansr.github.io/assets/img/portfolio/3d/3d_img1.png",
+                video: null,
+                description: null,
+                link: null,
+                tag:[ ]
+            },
+            {
+                name: "Design 3D",
+                type: "3d",
+                image: "https://harlansr.github.io/assets/img/portfolio/3d/3d_img2.png",
+                video: null,
+                description: null,
+                link: null,
+                tag:[ ]
+            },
+            {
+                name: "Design 3D",
+                type: "3d",
+                image: "https://harlansr.github.io/assets/img/portfolio/3d/3d_img3.png",
+                video: null,
+                description: null,
+                link: null,
+                tag:[ ]
+            },
+            {
+                name: "Design 3D",
+                type: "3d",
+                image: "https://harlansr.github.io/assets/img/portfolio/3d/3d_img4.png",
+                video: null,
+                description: null,
+                link: null,
+                tag:[ ]
+            },
+            {
+                name: "Design 3D",
+                type: "3d",
+                image: "https://harlansr.github.io/assets/img/portfolio/3d/vas_bunga6.png",
+                video: null,
+                description: null,
+                link: null,
+                tag:[ ]
+            },
+            {
+                name: "Playmate",
+                type: "ui/ux",
+                image: "https://harlansr.github.io/assets/img/portfolio/UIUX/ux_playmate.png",
+                video: null,
+                description: null,
+                link: null,
+                tag:[ ]
+            },
+            {
+                name: "Resporant",
+                type: "ui/ux",
+                image: "https://harlansr.github.io/assets/img/portfolio/UIUX/ux_restorant.png",
+                video: null,
+                description: null,
+                link: null,
+                tag:[ ]
+            },
+            {
+                name: "E-Print",
+                type: "ui/ux",
+                image: "https://harlansr.github.io/assets/img/portfolio/UIUX/ux_print.png",
+                video: null,
+                description: null,
+                link: null,
+                tag:[ ]
+            },
+        ],
+    },
+    
+}
